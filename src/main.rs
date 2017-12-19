ed0ccb3032c6130abd6b050727217462727a7465
Serhii Plyhun <snuk188@gmail.com>
2017-12-19 16:59:14 +0100

    Markup callbacks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate plygui;
-extern crate plygui_markup;
 
-use plygui_markup::*;
+use plygui::{UiButton, UiApplication};
+use plygui::markup::{self, MarkupRegistry, CallbackPtr};
 
 const TEST: &str = r#"
 	{
@@ -13,24 +13,31 @@ const TEST: &str = r#"
 		"children": [{
 				"id": "btn_click_it",
 				"type": "Button",
-				"label": "Click It"
+				"label": "Click It",
+				"on_left_click": "click_da_buttn"
 			},
 			{
 				"id": "btn_dont_click_it",
 				"type": "Button",
-				"label": "Don't Click It"
+				"label": "Don't Click It",
+				"on_left_click": "click_da_buttn"
 			}
 		]
 	}"#;
+	
+fn click_da_buttn(button: &mut UiButton) {
+	println!("{} bein clickd", button.label());
+}
 
 fn main() {
-	use plygui::{UiApplication};
+	let mut registry = MarkupRegistry::new();
+	plygui::register_markup_members(&mut registry);
+	registry.bind_callback("click_da_buttn", click_da_buttn as CallbackPtr).unwrap();
 	
 	let mut application = plygui::Application::with_name("Plygui markup test");
 	
 	let mut window = application.new_window("plygui!!", plygui::WindowStartSize::Exact(640, 480), false);
-	plygui::register_markup_members(plygui_markup::registry_mut());
-	let res = parse_markup(TEST);
+	let res = markup::parse_markup(TEST, &mut registry);
 	
 	window.set_child(Some(res));
 	application.start();
