0f4ba838b67b5b1a3846d66253254a8e69358a41
Serhii Plyhun <snuk188@gmail.com>
2017-12-20 10:30:33 +0100

    Non-working markup sample

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,13 @@ fn main() {
 	plygui::register_markup_members(&mut registry);
 	registry.bind_callback("click_da_buttn", click_da_buttn as CallbackPtr).unwrap();
 	
+	// Closures do not work :(
+	//let click_da_other_buttn = Box::new(|button: &mut UiButton| {println!("{} bein super clickd", button.label());});
+	//registry.bind_callback("click_da_other_buttn", click_da_other_buttn.as_ref() as &_ as *const _ as CallbackPtr).unwrap();
+	
+	//https://github.com/rust-lang/rust/issues/29638
+	//bind_markup_callback!(registry, click_da_buttn);
+	
 	let mut application = plygui::Application::with_name("Plygui markup test");
 	
 	let mut window = application.new_window("plygui!!", plygui::WindowStartSize::Exact(640, 480), false);
