ca8b90b427d28a98afc549faf2ca02fee5dff66b
Serhii Plyhun <snuk188@gmail.com>
2017-12-21 17:40:10 +0100

    Closure callbacks (experimental!)

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ const TEST: &str = r#"
 				"id": "btn_dont_click_it",
 				"type": "Button",
 				"label": "Don't Click It",
-				"on_left_click": "click_da_buttn"
+				"on_left_click": "click_da_other_buttn"
 			}
 		]
 	}"#;
@@ -32,11 +32,15 @@ fn click_da_buttn(button: &mut UiButton) {
 fn main() {
 	let mut registry = MarkupRegistry::new();
 	plygui::register_markup_members(&mut registry);
-	registry.bind_callback("click_da_buttn", click_da_buttn as CallbackPtr).unwrap();
+	
+	let click_da_buttn: Box<Box<FnMut(&mut UiButton)>> = Box::new(Box::new(click_da_buttn));
+	
+	registry.bind_callback("click_da_buttn", Box::into_raw(click_da_buttn) as *const _ as CallbackPtr).unwrap();
 	
 	// Closures do not work :(
-	//let click_da_other_buttn = Box::new(|button: &mut UiButton| {println!("{} bein super clickd", button.label());});
-	//registry.bind_callback("click_da_other_buttn", click_da_other_buttn.as_ref() as &_ as *const _ as CallbackPtr).unwrap();
+	let click_da_other_buttn: Box<Box<FnMut(&mut UiButton)>> = Box::new(Box::new(|button: &mut UiButton| {println!("{} bein super clickd", button.label());}));
+	let cb = Box::into_raw(click_da_other_buttn) as *const _ as CallbackPtr;
+	registry.bind_callback("click_da_other_buttn", cb).unwrap();
 	
 	//https://github.com/rust-lang/rust/issues/29638
 	//bind_markup_callback!(registry, click_da_buttn);
