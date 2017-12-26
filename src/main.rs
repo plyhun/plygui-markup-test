b276fa5b6daaadad062e7e26ccce4c73c2bd9e24
snuk182 <snuk188@gmail.com>
2017-12-26 20:21:28 +0100

    Callbacks work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate plygui;
 
 use plygui::{UiButton, UiApplication};
-use plygui::markup::{self, MarkupRegistry, CallbackPtr};
+use plygui::markup::{self, MarkupRegistry};
+use plygui::callbacks;
 
 const TEST: &str = r#"
 	{
@@ -33,14 +34,8 @@ fn main() {
 	let mut registry = MarkupRegistry::new();
 	plygui::register_markup_members(&mut registry);
 	
-	let click_da_buttn: Box<Box<FnMut(&mut UiButton)>> = Box::new(Box::new(click_da_buttn));
-	
-	registry.bind_callback("click_da_buttn", Box::into_raw(click_da_buttn) as *const _ as CallbackPtr).unwrap();
-	
-	// Closures do not work :(
-	let click_da_other_buttn: Box<Box<FnMut(&mut UiButton)>> = Box::new(Box::new(|button: &mut UiButton| {println!("{} bein super clickd", button.label());}));
-	let cb = Box::into_raw(click_da_other_buttn) as *const _ as CallbackPtr;
-	registry.bind_callback("click_da_other_buttn", cb).unwrap();
+	registry.push_callback("click_da_buttn", callbacks::Click::from(click_da_buttn)).unwrap();
+	registry.push_callback("click_da_other_buttn", callbacks::Click::from(|button: &mut UiButton| {println!("{} bein super clickd", button.label());})).unwrap();
 	
 	//https://github.com/rust-lang/rust/issues/29638
 	//bind_markup_callback!(registry, click_da_buttn);
